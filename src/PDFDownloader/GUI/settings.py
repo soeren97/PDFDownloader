681ed6b552a865fecfcd5c37c695c4436fd8511b
Søren <soeren@langkilde.com>
2024-04-17 14:31:16 +0200

    Added functionality to change other constants

--- src/PDFDownloader/GUI/settings.py
+++ src/PDFDownloader/GUI/settings.py
@@ -53,18 +53,57 @@ class SettingsWindow:
         )
         self.nsa_button.grid(row=1, column=2, padx=5, pady=5)
 
+        self.downloaded_label = tk.Label(
+            self.setup_window, text="Downloadet checklist:"
+        )
+        self.downloaded_label.grid(row=2, column=0, padx=10, pady=5)
+
+        self.downloaded_entry = tk.Entry(self.setup_window)
+        self.downloaded_entry.grid(row=2, column=1, padx=10, pady=5)
+        self.downloaded_entry.insert(
+            0, self.constant_handler.constants["Downloaded_file"]
+        )
+
+        self.downloaded_button = tk.Button(
+            self.setup_window, text="Find", command=self.browse_downloaded
+        )
+        self.downloaded_button.grid(row=2, column=2, padx=5, pady=5)
+
+        self.index1_label = tk.Label(self.setup_window, text="PDF URL kolonne nummer:")
+        self.index1_label.grid(row=3, column=0, padx=10, pady=5)
+
+        self.index1_entry = tk.Entry(self.setup_window)
+        self.index1_entry.grid(row=3, column=1, padx=10, pady=5)
+        self.index1_entry.insert(0, self.constant_handler.constants["URL_index1"])
+
+        self.index2_label = tk.Label(
+            self.setup_window, text="Report html kolonne nummer:"
+        )
+        self.index2_label.grid(row=4, column=0, padx=10, pady=5)
+
+        self.index2_entry = tk.Entry(self.setup_window)
+        self.index2_entry.grid(row=4, column=1, padx=10, pady=5)
+        self.index2_entry.insert(0, self.constant_handler.constants["URL_index2"])
+
+        self.timeout_label = tk.Label(self.setup_window, text="Timeout (s):")
+        self.timeout_label.grid(row=5, column=0, padx=10, pady=5)
+
+        self.timeout_entry = tk.Entry(self.setup_window)
+        self.timeout_entry.grid(row=5, column=1, padx=10, pady=5)
+        self.timeout_entry.insert(0, self.constant_handler.constants["Timeout"])
+
         self.save_button = tk.Button(
             self.setup_window, text="Gem ændringer", command=self.save_changes
         )
         self.save_button.grid(
-            row=2, column=0, columnspan=3, padx=10, pady=5, sticky="we"
+            row=6, column=0, columnspan=3, padx=10, pady=5, sticky="we"
         )
 
         self.back_button = tk.Button(
             self.setup_window, text="Tilbage", command=self.close
         )
         self.back_button.grid(
-            row=3, column=0, columnspan=3, padx=10, pady=5, sticky="we"
+            row=7, column=0, columnspan=3, padx=10, pady=5, sticky="we"
         )
 
     def browse_excel(self) -> None:
@@ -75,6 +114,15 @@ class SettingsWindow:
         if filename:
             self.excel_entry.delete(0, tk.END)
             self.excel_entry.insert(tk.END, filename)
+
+    def browse_downloaded(self) -> None:
+        """Select location of downloaded file."""
+        filename = filedialog.askopenfilename(
+            filetypes=(("Excel files", "*.xlsx"), ("All files", "*.*"))
+        )
+        if filename:
+            self.downloaded_entry.delete(0, tk.END)
+            self.downloaded_entry.insert(tk.END, filename)
 
     def browse_nsa(self) -> None:
         """Select location of NSA."""
@@ -87,11 +135,19 @@ class SettingsWindow:
         """Save changes made."""
         excel_location = self.excel_entry.get()
         nsa_location = self.nsa_entry.get()
+        downloaded_location = self.downloaded_entry.get()
+        index1 = self.index1_entry.get()
+        index2 = self.index2_entry.get()
+        timeout = self.timeout_entry.get()
 
         constants = self.constant_handler.constants
 
         constants["Data_file"] = excel_location
         constants["PDF_folder"] = nsa_location
+        constants["Downloaded_file"] = downloaded_location
+        constants["URL_index1"] = index1
+        constants["URL_index2"] = index2
+        constants["Timeout"] = timeout
 
         self.constant_handler.update_constants(constants)
 
